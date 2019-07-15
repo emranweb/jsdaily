cce4471973d8cfd78b5d0d330bcf8bb1dde0d6a0
emranweb <emranbd77@gmail.com>
2019-07-15 19:17:38 +0600

    tab update

--- tab/app.js
+++ tab/app.js
@@ -1,18 +1,15 @@
 const list = document.querySelectorAll('.tab-list a');
 
-for (let item of list){
-    item.addEventListener("click", tab);
+for (let item of list) {
+  item.addEventListener("click", tabFunction);
 }
-function tab(){
-    let a  = this.getAttribute("href").slice(1);
-    console.log(a)
-    
-    let b =document.querySelectorAll('.tab-content');
-    let c = Array.from(b);
-   let x= c.filter(item=>{
-      return (item.getAttribute('id')=== a);
-    });
-    y= document.querySelector('.tab-content.active');
-    y.classList.remove("active");
-    x[0].classList.add('active');
+
+function tabFunction() {
+  let tabBtn = this.getAttribute("href").slice(1);
+
+  let tabContent = Array.from(document.querySelectorAll('.tab-content'));
+  let tabFilter = tabContent.filter(item => (item.getAttribute('id') === tabBtn));
+  let tabActive = document.querySelector('.tab-content.active');
+  tabActive.classList.remove("active");
+  tabFilter[0].classList.add('active');
 }
