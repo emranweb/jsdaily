299460ca24e5627b4907354d59263880ecc4c14e
emranweb <emranbd77@gmail.com>
2020-11-09 00:17:52 +0600

    iretator fun

--- iterable/iterate.js
+++ iterable/iterate.js
@@ -3,34 +3,23 @@ const range = {
   to: 5,
 };
 
-range[Symbol.iterator] = function () {
+
+range[Symbol.iterator] = function (){
+  
   return {
-    current: this.from,
-    last: this.to,
-
-    next() {
-      if (this.current <= this.last) {
-        return {
-          done: false,
-          value: this.current++,
-        };
-      } else {
-        return {
-          done: true,
-        };
-      }
-    },
-  };
-};
-
-
-const person= ['A',"B","C","D"]
-
-const item  = person[Symbol.iterator]();
-
-while(true){
-  let a = item.next();
-  if(a.done){
-    break;
+     current : this.from,
+     last: this.to,
+     next(){
+        if(this.current <= this.last){
+          return {done:false, value:this.current++}
+        }else{
+           return {done:true}
+        }
+     }
   }
+
+}
+
+for (let num of range){
+   console.log(num)
 }
