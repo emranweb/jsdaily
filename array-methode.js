f90ff60ae1b98bbbebf0a1d372f1e14e14526d5f
emranweb <emranbd77@gmail.com>
2020-11-16 15:12:36 +0600

    js

--- array-methode.js
+++ array-methode.js
@@ -1,27 +1,18 @@
 const ar = [1, 2, 3];
 
-
 //array prototype map
-let newAr = ar.map(x => x * 2);
-console.log(newAr)
-
-
+let newAr = ar.map((x) => x * 2);
+console.log(newAr);
 
 //array prototype filter
-const newFilter = ar.filter(x => x % 2 === 1);
-console.log(newFilter)
-
+const newFilter = ar.filter((x) => x % 2 === 1);
+console.log(newFilter);
 
 //array prototype reduce
 const sum = ar.reduce((accoumulation, current) => {
-    return accoumulation + current;
-    console.log(accoumulation);
-    console.log(current)
+  return accoumulation + current;
 });
 
-console.log(sum)
-
-
 // array prototype find
-const odd = ar.find(x=>x%2===1);
-console.log(odd)
+const odd = ar.find((x) => x % 2 === 1);
+console.log(odd);
